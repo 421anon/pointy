50ba3aff3a21016ffd38916817d792031dba6fde
hunor <hunorgered@protonmail.com>
2026-07-17 17:44:35 +0200

    support commits without presets

--- backend/src/Handlers/Presets.hs
+++ backend/src/Handlers/Presets.hs
@@ -7,17 +7,17 @@ import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TLE
 import Servant (Handler, throwError)
 import Servant.Server (err500, errBody)
-import UserRepo (ReadRepoContext (..), runNixEvalJsonInRepo, withReadRepoTransaction)
+import UserRepo (ReadRepoContext (..), runNixEvalJsonApplyInRepo, withReadRepoTransaction)
 
 getPresetsHandler :: Maybe Text -> Handler DynamicJson
 getPresetsHandler mCommit = do
+    let evalPresets ctx = do
+            output <- runNixEvalJsonApplyInRepo ctx "pointy: pointy.presets or {}" "#pointy"
+            return (TLE.encodeUtf8 (TL.pack output))
     result <- liftIO $ case mCommit of
-        Just commit -> withReadRepoTransaction $ \(ReadRepoContext repoPath _) -> do
-            output <- runNixEvalJsonInRepo (ReadRepoContext repoPath $ unpack commit) "#pointy.presets"
-            return (TLE.encodeUtf8 (TL.pack output))
-        Nothing -> withReadRepoTransaction $ \ctx -> do
-            output <- runNixEvalJsonInRepo ctx "#pointy.presets"
-            return (TLE.encodeUtf8 (TL.pack output))
+        Just commit -> withReadRepoTransaction $ \(ReadRepoContext repoPath _) ->
+            evalPresets (ReadRepoContext repoPath $ unpack commit)
+        Nothing -> withReadRepoTransaction evalPresets
     case result of
         Right output -> return (DynamicJson output)
         Left err -> throwError $ err500{errBody = TLE.encodeUtf8 (TL.pack err)}
